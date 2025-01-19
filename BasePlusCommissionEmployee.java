87e559e82b7a87e981aabc8077e21318f8b5edeb
Psammy <ampadupsamuel1@gmail.com>
2025-01-19 23:51:58 +0000

    Update

--- BasePlusCommissionEmployee.java
+++ BasePlusCommissionEmployee.java
@@ -3,7 +3,10 @@ package Tasks;
 public class BasePlusCommissionEmployee {
     private double baseSalary;
 
-    public BasePlusCommissionEmployee(String firstname,String lastName,String socialSecurityNumber,double gorssSSales, double commissionRate){
-        
+    public BasePlusCommissionEmployee(String firstname,String lastName,String socialSecurityNumber,double gorssSSales, double commissionRate,double baseSalary){
+    
     }
 }
+
+
+
