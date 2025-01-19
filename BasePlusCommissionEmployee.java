e9f8daa64a0baa14287bacd07372d9e3363a2475
Psammy <ampadupsamuel1@gmail.com>
2025-01-19 23:55:58 +0000

    Update

--- BasePlusCommissionEmployee.java
+++ BasePlusCommissionEmployee.java
@@ -2,10 +2,19 @@ package Tasks;
 
 public class BasePlusCommissionEmployee extends CommissionEmployee{
     private double baseSalary;
+    
+    public double getBaseSalary() {
+        return baseSalary;
+    }
+    public void setBaseSalary(double baseSalary) {
+        this.baseSalary = baseSalary;
+    }
+
+
 
     public BasePlusCommissionEmployee(String firstName,String lastName,String socialSecurityNumber,double grossSSales, double commissionRate,double baseSalary){
         super(firstName,lastName,socialSecurityNumber,grossSSales,commissionRate);
-        this.baseSalary=baseSalary;
+
     }
 }
 
