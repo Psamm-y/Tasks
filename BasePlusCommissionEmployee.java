f4ce5adaea754fd0935075e36f54e83c0fb4b844
Psammy <ampadupsamuel1@gmail.com>
2025-01-20 20:50:35 +0000

    Update

--- BasePlusCommissionEmployee.java
+++ BasePlusCommissionEmployee.java
@@ -4,11 +4,17 @@ public class BasePlusCommissionEmployee extends CommissionEmployee{
     private double baseSalary;
 
     public double getBaseSalary() {
+
         return baseSalary;
     }
     public void setBaseSalary(double baseSalary) {
+        if(baseSalary<=0){
+            throw new IllegalArgumentException("Base Salary must be greater than 0");
+        }
         this.baseSalary = baseSalary;
     }
+
+
 
     public BasePlusCommissionEmployee(String firstName,String lastName,String socialSecurityNumber,double grossSSales, double commissionRate,double baseSalary){
         super(firstName,lastName,socialSecurityNumber,grossSSales,commissionRate); //initialize inherited fields from superClass
