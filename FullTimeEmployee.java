7e8a8a028f860da1ddea16e9927aa1cdcb2d6fae
Psammy <ampadupsamuel1@gmail.com>
2025-01-25 23:59:35 +0000

    Update

--- FullTimeEmployee.java
+++ FullTimeEmployee.java
@@ -2,9 +2,14 @@ package Tasks;
 
 public class FullTimeEmployee extends Employee{
 
+    private double salary;
+    public double getSalary() {
+        return salary;
+    }
 
-    public FullTimeEmployee(String name, String employeeId) {
+    public FullTimeEmployee(String name, String employeeId,double salary) {
         super(name, employeeId);
+        this.salary= salary;
     }
 
     public double calculatePay() {
