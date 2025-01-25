dce61894507d9a82a0e26a29db26872bd0d3267f
Psammy <ampadupsamuel1@gmail.com>
2025-01-25 23:40:06 +0000

    Update

--- Employee.java
+++ Employee.java
@@ -3,5 +3,8 @@ package Tasks;
 public abstract class Employee {
     private String name;
     private String employeeId;
-    
+
+    public Employee(String name, String employeeId){
+        
+    }
 }
