6d4775a814bfaa39617ec2d5972a292f652162db
Psammy <ampadupsamuel1@gmail.com>
2025-01-25 23:49:35 +0000

    Update

--- Employee.java
+++ Employee.java
@@ -1,10 +1,20 @@
 package Tasks;
 
 public abstract class Employee {
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
     private String name;
     private String employeeId;
 
     public Employee(String name, String employeeId){
-        
+        this.name=name;
     }
+    public abstract double calculatePay();
+
 }
--- FullTimeEmployee.java
+++ FullTimeEmployee.java
@@ -1,5 +1,5 @@
 package Tasks;
 
-public abstract class FullTimeEmployee {
+public class FullTimeEmployee extends Employee() {
     
 }
